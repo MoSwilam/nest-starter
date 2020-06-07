c8aa5566b11aaff1ac1766a352e266bcd7317ec5
MoSwilam <muswilam@gmail.com>
2020-06-07 10:25:28 +0300

    db ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/exception-filter/http.exception.filter';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forRoot()],
   providers: [
     {
       provide: APP_PIPE,
