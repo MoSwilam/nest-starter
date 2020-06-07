42556e252f37bfc5c0cd12ffe29ee4dea33f357a
MoSwilam <muswilam@gmail.com>
2020-06-07 22:23:29 +0300

    github

--- src/modules/idea/schema/idea.schema.ts
+++
@@ -1,8 +0,0 @@
-import {object, number, string, ObjectSchema } from '@hapi/joi';
-import { id } from '../../../common/validationSchemas/common.validation.schemas';
-
-export const addIdeaSchema: ObjectSchema = object({
-    idea: string().min(3).max(100),
-    description: string().min(3).max(100).required(),
-})
-
